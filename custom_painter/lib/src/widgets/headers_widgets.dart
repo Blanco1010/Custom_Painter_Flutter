4e575c2f43592074b38a23b222adbbc173c42625
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-07-26 15:27:55 -0500

    add the headerBox

---
+++ custom_painter/lib/src/widgets/headers_widgets.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HeaderBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      color: Color(0xff615AAB),
+    );
+  }
+}
