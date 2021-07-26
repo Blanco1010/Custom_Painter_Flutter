cc6b2619996caea993b7e273e39c864ff19a3800
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-07-26 15:42:30 -0500

    add HeaderBorderCircle for this app

--- custom_painter/lib/src/widgets/headers_widgets.dart
+++ custom_painter/lib/src/widgets/headers_widgets.dart
@@ -9,3 +9,19 @@ class HeaderBox extends StatelessWidget {
     );
   }
 }
+
+class HeaderBorderCircle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(70),
+          bottomRight: Radius.circular(70),
+        ),
+        color: Color(0xff615AAB),
+      ),
+      height: 250,
+    );
+  }
+}
