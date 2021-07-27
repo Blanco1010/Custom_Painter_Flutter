ce4dc1aa2db4316ac4645be297fb5acc41de7617
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-07-27 15:25:24 -0500

    add animationController

--- custom_painter/lib/src/pages/animation_page.dart
+++ custom_painter/lib/src/pages/animation_page.dart
@@ -16,10 +16,43 @@ class BoxAnimation extends StatefulWidget {
   _BoxAnimationState createState() => _BoxAnimationState();
 }
 
-class _BoxAnimationState extends State<BoxAnimation> {
+class _BoxAnimationState extends State<BoxAnimation>
+    with SingleTickerProviderStateMixin {
+  AnimationController? controller;
+
+  Animation<double>? rotation;
+
+  @override
+  void initState() {
+    controller = new AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    rotation = Tween(begin: 0.0, end: 2.8).animate(controller!);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return _Rectangle();
+    //Play
+    controller!.forward();
+
+    return AnimatedBuilder(
+      animation: controller!,
+      // child: _Rectangle(),
+      builder: (BuildContext context, Widget? child) {
+        print(rotation!.value);
+        return Transform.rotate(angle: rotation!.value, child: _Rectangle());
+      },
+    );
   }
 }
 
