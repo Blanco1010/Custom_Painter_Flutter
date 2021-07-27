c2ceb5ade0c042525650b8c14d18b2e4478a34cf
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-07-27 11:40:07 -0500

    add HeaderWavesPainter

--- custom_painter/lib/src/widgets/headers_widgets.dart
+++ custom_painter/lib/src/widgets/headers_widgets.dart
@@ -194,3 +194,58 @@ class _HeaderCurvePainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderWaves extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderWavesPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavesPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint();
+
+    //paint properties
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 20;
+
+    //Draw with paint
+    final path = new Path();
+
+    path.moveTo(0, 0);
+    path.lineTo(0, size.height * 0.30);
+
+    path.quadraticBezierTo(
+      size.width * 0.25,
+      size.width * 0.75,
+      size.width * 0.5,
+      size.height * 0.3,
+    );
+
+    path.quadraticBezierTo(
+      size.width * 0.75,
+      size.height * 0.23,
+      size.width,
+      size.height * 0.3,
+    );
+
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
