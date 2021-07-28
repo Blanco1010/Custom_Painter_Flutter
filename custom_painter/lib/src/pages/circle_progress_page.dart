17eae3d872d550e6e553ce24dea02fc29af3dabd
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-07-28 18:26:38 -0500

    add BawAngle for this circle

--- custom_painter/lib/src/pages/circle_progress_page.dart
+++ custom_painter/lib/src/pages/circle_progress_page.dart
@@ -12,11 +12,12 @@ class _CircleProgressPageState extends State<CircleProgressPage> {
     return Scaffold(
       body: Center(
         child: Container(
+          padding: EdgeInsets.all(10),
           width: 300,
           height: 300,
-          color: Colors.red,
+          //color: Colors.red,
           child: CustomPaint(
-            painter: _MyradialProgress(),
+            painter: _MyradialProgress(50),
           ),
         ),
       ),
@@ -25,17 +26,40 @@ class _CircleProgressPageState extends State<CircleProgressPage> {
 }
 
 class _MyradialProgress extends CustomPainter {
+  final percentaje;
+
+  _MyradialProgress(this.percentaje);
+
   @override
   void paint(Canvas canvas, Size size) {
+    //Draw a circle
     final paint = new Paint();
-    paint.strokeWidth = 4;
+    paint.strokeWidth = 5;
     paint.color = Colors.black;
     paint.style = PaintingStyle.stroke;
 
     final center = Offset(size.width * 0.5, size.height * 0.5);
-    double radius = min(size.width * 0.5, size.height * 0.5);
+    final radius = min(size.width * 0.5, size.height * 0.5);
 
     canvas.drawCircle(center, radius, paint);
+
+    //Baw
+
+    final paintBaw = new Paint();
+    paintBaw.strokeWidth = 10;
+    paintBaw.color = Colors.pink;
+    paintBaw.style = PaintingStyle.stroke;
+
+    //Part to fill out
+
+    final bawAngle = 2 * pi * (percentaje / 100);
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      -pi / 2,
+      bawAngle,
+      false,
+      paintBaw,
+    );
   }
 
   @override
