2751f725a69441771a6557de12a7bbc4c16b448d
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-07-31 19:04:15 -0500

    add the slides and dots

--- custom_painter/lib/src/pages/slideshow_page.dart
+++ custom_painter/lib/src/pages/slideshow_page.dart
@@ -1,12 +1,92 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_svg/flutter_svg.dart';
+
 class SladeShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+      body: Column(
+        children: [
+          Expanded(
+            child: _Slides(),
+          ),
+          _Dots(),
+        ],
       ),
+    );
+  }
+}
+
+// SvgPicture.asset('assets/svgs/slide-1.svg')
+
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          _Slide(svg: 'assets/svgs/slide-1.svg'),
+          _Slide(svg: 'assets/svgs/slide-2.svg'),
+          _Slide(svg: 'assets/svgs/slide-3.svg'),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.08,
+      //color: Colors.red,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Dot(size: size),
+          _Dot(size: size),
+          _Dot(size: size),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  const _Dot({required this.size});
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size.width * 0.04,
+      height: size.height * 0.04,
+      margin: EdgeInsets.symmetric(horizontal: size.width * 0.02),
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  const _Slide({required this.svg});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      padding: EdgeInsets.all(30),
+      child: SvgPicture.asset(svg),
     );
   }
 }
