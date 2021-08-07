626dcb221146386c8169487c513c92626f7104ba
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-08-07 11:02:52 -0500

    add the buttons

--- custom_painter/lib/src/pages/emergency_page.dart
+++ custom_painter/lib/src/pages/emergency_page.dart
@@ -3,19 +3,118 @@ import 'package:custom_painter/src/widgets/button_fat.dart';
 import 'package:custom_painter/src/widgets/headers_widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+class ItemBoton {
+  final IconData icon;
+  final String text;
+  final Color color1;
+  final Color color2;
+
+  ItemBoton(this.icon, this.text, this.color1, this.color2);
+}
+
+final items = <ItemBoton>[
+  new ItemBoton(
+    FontAwesomeIcons.carCrash,
+    'Motor Accident',
+    Color(0xff6989F5),
+    Color(0xff906EF5),
+  ),
+  new ItemBoton(
+    FontAwesomeIcons.plus,
+    'Medical Emergency',
+    Color(0xff66A9F2),
+    Color(0xff536CF6),
+  ),
+  new ItemBoton(
+    FontAwesomeIcons.theaterMasks,
+    'Theft / Harrasement',
+    Color(0xffF2D572),
+    Color(0xffE06AA3),
+  ),
+  new ItemBoton(
+    FontAwesomeIcons.biking,
+    'Awards',
+    Color(0xff317183),
+    Color(0xff46997D),
+  ),
+  new ItemBoton(
+    FontAwesomeIcons.carCrash,
+    'Motor Accident',
+    Color(0xff6989F5),
+    Color(0xff906EF5),
+  ),
+  new ItemBoton(
+    FontAwesomeIcons.plus,
+    'Medical Emergency',
+    Color(0xff66A9F2),
+    Color(0xff536CF6),
+  ),
+  new ItemBoton(
+    FontAwesomeIcons.theaterMasks,
+    'Theft / Harrasement',
+    Color(0xffF2D572),
+    Color(0xffE06AA3),
+  ),
+  new ItemBoton(
+    FontAwesomeIcons.biking,
+    'Awards',
+    Color(0xff317183),
+    Color(0xff46997D),
+  ),
+  new ItemBoton(
+    FontAwesomeIcons.carCrash,
+    'Motor Accident',
+    Color(0xff6989F5),
+    Color(0xff906EF5),
+  ),
+  new ItemBoton(
+    FontAwesomeIcons.plus,
+    'Medical Emergency',
+    Color(0xff66A9F2),
+    Color(0xff536CF6),
+  ),
+  new ItemBoton(
+    FontAwesomeIcons.theaterMasks,
+    'Theft / Harrasement',
+    Color(0xffF2D572),
+    Color(0xffE06AA3),
+  ),
+  new ItemBoton(
+    FontAwesomeIcons.biking,
+    'Awards',
+    Color(0xff317183),
+    Color(0xff46997D),
+  ),
+];
+
+List<Widget> itemMap = items
+    .map((e) => ButtonFat(
+          icon: e.icon,
+          title: e.text,
+          color: e.color1,
+          color2: e.color2,
+          onPressed: () {},
+        ))
+    .toList();
+
 class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ButtonFat(
-            icon: FontAwesomeIcons.carCrash,
-            title: 'Motor Accident',
-            color: Color(0Xff6989F5),
-            color2: Color(0Xff906EF5),
-            onPressed: () {
-              print('Car Accident');
-            }),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 200),
+            child: ListView(
+              physics: BouncingScrollPhysics(),
+              children: <Widget>[
+                SizedBox(height: 80),
+                ...itemMap,
+              ],
+            ),
+          ),
+          HeaderPage(),
+        ],
       ),
     );
   }
